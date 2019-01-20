b19c6096207fff0bb7659f5a1aa3413d4f17bf8c
Braden Frazer <bradenfrazer@gmail.com>
2019-01-20 14:03:55 -0600

    build script v4

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ mkdir -p "$DEST/"
 # Execute all php files and save them as html
 for f in *.php;
 do
-    php $f | sed 's:\(<a.*href=".*\)\.php\(".*</a>\):\1\.html\2:g' > "$DEST/${f/.php/$html}";
-    echo "Processing $f into ${f/.php/$html}..";
+	php $f | sed 's:\(<a.*href=".*\)\.php\(".*</a>\):\1\.html\2:g' > "$DEST/${f/.php/$html}";
+	echo "Processing $f into ${f/.php/$html}..";
 done
 
 
