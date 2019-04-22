b742fd386469bfb30d85a056c48956dcb8e1a45a
Braden Frazer <bradenfrazer@gmail.com>
2019-04-22 18:36:37 -0500

    initial animations implementation with animations.css

--- header.php
+++ header.php
@@ -11,7 +11,8 @@
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab|Roboto:400,700|Caveat+Brush" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css" rel="stylesheet" text="text/css" media="screen">
-	<link href="/style.css" rel="stylesheet" text="text/css" media="screen">
+	<link href="/style.css" rel="stylesheet" text="text/css" media="screen"> <!--main styles-->
+	<link href="/animations.css" rel="stylesheet" text="text/css" media="screen"> <!--animation stylesheet-->
 
 	<!-- JavaScript -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
@@ -30,7 +31,7 @@
 <body>
 	<div class="container">
 		<nav class="navbar navbar-expand-lg navbar-light p-0">
-			<a class="navbar-brand" href="/">
+			<a class="navbar-brand hvr-buzz-out" href="/">
 					<picture>
   					<source media="(min-width: 768px)" srcset="/images/design/logoblack.png">
   					<img src="/images/design/logoblack-256px.png" alt="Braden Frazer logo"/>
@@ -52,12 +53,12 @@
 					</li>
 				</ul>
 				<div class="navbar-text socials">
-					<a class="nav-item nav-link" href="https://www.facebook.com/bradenmfrazer" target="_blank" rel="noopener">
+					<a class="nav-item nav-link hvr-bob" href="https://www.facebook.com/bradenmfrazer" target="_blank" rel="noopener">
 						<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50">
 							<path d="M32,11h5c0.552,0,1-0.448,1-1V3.263c0-0.524-0.403-0.96-0.925-0.997C35.484,2.153,32.376,2,30.141,2C24,2,20,5.68,20,12.368 V19h-7c-0.552,0-1,0.448-1,1v7c0,0.552,0.448,1,1,1h7v19c0,0.552,0.448,1,1,1h7c0.552,0,1-0.448,1-1V28h7.222 c0.51,0,0.938-0.383,0.994-0.89l0.778-7C38.06,19.518,37.596,19,37,19h-8v-5C29,12.343,30.343,11,32,11z"></path>
 						</svg>
 					</a>
-					<a class="nav-item nav-link" href="https://twitter.com/bradenfrazer" target="_blank" rel="noopener">
+					<a class="nav-item nav-link hvr-bob" href="https://twitter.com/bradenfrazer" target="_blank" rel="noopener">
 
 						<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50">
 							<g id="surface1">
@@ -66,21 +67,21 @@
 						</svg>
 
 					</a>
-					<a class="nav-item nav-link" href="https://www.instagram.com/bradenfrazer/" target="_blank" rel="noopener">
+					<a class="nav-item nav-link hvr-bob" href="https://www.instagram.com/bradenfrazer/" target="_blank" rel="noopener">
 
 						<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50">
 							<path d="M 16 3 C 8.83 3 3 8.83 3 16 L 3 34 C 3 41.17 8.83 47 16 47 L 34 47 C 41.17 47 47 41.17 47 34 L 47 16 C 47 8.83 41.17 3 34 3 L 16 3 z M 37 11 C 38.1 11 39 11.9 39 13 C 39 14.1 38.1 15 37 15 C 35.9 15 35 14.1 35 13 C 35 11.9 35.9 11 37 11 z M 25 14 C 31.07 14 36 18.93 36 25 C 36 31.07 31.07 36 25 36 C 18.93 36 14 31.07 14 25 C 14 18.93 18.93 14 25 14 z M 25 16 C 20.04 16 16 20.04 16 25 C 16 29.96 20.04 34 25 34 C 29.96 34 34 29.96 34 25 C 34 20.04 29.96 16 25 16 z"></path>
 						</svg>
 
 					</a>
-					<a class="nav-item nav-link" href="https://www.linkedin.com/in/bradenfrazer" target="_blank" rel="noopener">
+					<a class="nav-item nav-link hvr-bob" href="https://www.linkedin.com/in/bradenfrazer" target="_blank" rel="noopener">
 
 						<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50">
 							<path d="M 8 3.0097656 C 4.53 3.0097656 2.0097656 5.0892187 2.0097656 7.9492188 C 2.0097656 10.819219 4.59 12.990234 8 12.990234 C 11.47 12.990234 13.990234 10.870625 13.990234 7.890625 C 13.830234 5.020625 11.36 3.0097656 8 3.0097656 z M 3 15 C 2.45 15 2 15.45 2 16 L 2 45 C 2 45.55 2.45 46 3 46 L 13 46 C 13.55 46 14 45.55 14 45 L 14 16 C 14 15.45 13.55 15 13 15 L 3 15 z M 18 15 C 17.45 15 17 15.45 17 16 L 17 45 C 17 45.55 17.45 46 18 46 L 27 46 C 27.552 46 28 45.552 28 45 L 28 30 L 28 29.75 L 28 29.5 C 28 27.13 29.820625 25.199531 32.140625 25.019531 C 32.260625 24.999531 32.38 25 32.5 25 C 32.62 25 32.739375 24.999531 32.859375 25.019531 C 35.179375 25.199531 37 27.13 37 29.5 L 37 45 C 37 45.552 37.448 46 38 46 L 47 46 C 47.55 46 48 45.55 48 45 L 48 28 C 48 21.53 44.529063 15 36.789062 15 C 33.269062 15 30.61 16.360234 29 17.490234 L 29 16 C 29 15.45 28.55 15 28 15 L 18 15 z"></path>
 						</svg>
 
 					</a>
-					<a class="nav-item nav-link" href="https://github.com/bradenfrazer/" target="_blank" rel="noopener">
+					<a class="nav-item nav-link hvr-bob" href="https://github.com/bradenfrazer/" target="_blank" rel="noopener">
 
 						<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 30 30" width="30" height="30">
 							<path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z" />
